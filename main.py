d142ed069fde51e70303756370381172800a18ff
Sachin Tanwar <Sachin.Tanwar@thrivepass.com>
2025-04-04 15:17:02 +0530

    added new tables

--- main.py
+++ main.py
@@ -14,5 +14,12 @@ supabase: Client = create_client(url, key)
 # Fetch user
 @app.get("/user")
 def get_user(last4ssn: int, dob: date, zip_code: int):
-    user = supabase.from_("users").select("*, user_payments(*), user_coverages(*)").eq("last4ssn", last4ssn).eq("dob", dob).eq("zip_code", zip_code).execute()
+    user = (
+        supabase.from_("users")
+        .select("name, user_recent_payments(amount, received_date, method, coverage_month), user_pending_payments(amount, due_date, coverage_month), user_coverages(monthly_premium, user_coverage_plans(plan_name, coverage))")
+        .eq("last4ssn", last4ssn)
+        .eq("dob", dob)
+        .eq("zip_code", zip_code)
+        .execute()
+    )
     return user.data
